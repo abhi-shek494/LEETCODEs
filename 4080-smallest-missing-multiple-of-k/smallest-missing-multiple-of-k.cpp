b101f63a03ff207d2d3b791518b3372d3b292588
abhi-shek494 <abhishekun49@gmail.com>
2026-08-25 22:48:18 +0530

    Time: 0 ms (100.00%) | Memory: 25.1 MB (45.06%) - LeetSync

---
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingMultiple(vector<int>& nums, int k) {
+        unordered_set<int> st(nums.begin(), nums.end());
+        for(int i=1; ; i++){
+            int x=k*i;
+            if(!st.count(x)){
+                return x;
+            }
+        }
+    }
+};
